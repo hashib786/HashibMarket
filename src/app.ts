7110c7c1a665b7d716fc22c744c8b7bcf1800fe3
Hashib Raja <hashibraja23@gmail.com>
2023-09-04 14:46:00 +0530

    added morgan middleware for who is requesting our app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import express from "express";
 
 import userRouter from "./routes/userRoutes";
 import { errorController } from "./controller/errorController";
+import morgan from "morgan";
 
 const app = express();
 
 // Middlewares
 app.use(express.json({ limit: "10kb" }));
+
+// Logging console who is requested our app
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use("/api/v1/users", userRouter);
 
