786c5afd39dc17907a92be5fc990bbd7a4e46d63
Hashib Raja <hashibraja23@gmail.com>
2023-09-07 06:54:16 +0530

    in cloudenery upload image in specific folder

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,22 +1,29 @@
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../utils/catchAsync";
 import { AppError } from "../utils/AppError";
-import { v2 as cloudinaryV2, UploadApiResponse } from "cloudinary"; // Import `UploadApiResponse`
+import {
+  v2 as cloudinaryV2,
+  UploadApiOptions,
+  UploadApiResponse,
+} from "cloudinary"; // Import `UploadApiResponse`
 import { Readable } from "stream";
 
 const bufferUpload = async (
   buffer: Buffer
 ): Promise<UploadApiResponse | undefined> => {
   return new Promise((resolve, reject) => {
-    const writeStream = cloudinaryV2.uploader.upload_stream(
-      {
-        transformation: {
-          aspect_ratio: "1.0",
-          gravity: "face",
-          width: 150,
-          crop: "fill",
-        },
+    const uploadOption: UploadApiOptions = {
+      transformation: {
+        aspect_ratio: "1.0",
+        gravity: "face",
+        width: 150,
+        crop: "fill",
       },
+      folder: "users",
+    };
+
+    const writeStream = cloudinaryV2.uploader.upload_stream(
+      uploadOption,
       (err, result) => {
         if (err) {
           reject(err);
@@ -25,6 +32,7 @@ const bufferUpload = async (
         resolve(result);
       }
     );
+
     const readStream = new Readable({
       read() {
         this.push(buffer);
