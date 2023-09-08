7e55341b3a747a57f54f69afc807c0ebcbf991a3
Hashib Raja <hashibraja23@gmail.com>
2023-09-08 18:24:57 +0530

    before update product checking seller is created witch one is updating

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import Product from "../models/productModel";
 import { createOne, getAll, getOne, updateOne } from "./handleFactory";
+import { catchAsync } from "../utils/catchAsync";
+import { AppError } from "../utils/AppError";
 
 export const setSellerId = (req: Request, _: Response, next: NextFunction) => {
   req.body.seller = req.user._id;
@@ -10,4 +12,20 @@ export const setSellerId = (req: Request, _: Response, next: NextFunction) => {
 export const getAllProducts = getAll(Product);
 export const createProduct = createOne(Product);
 export const getProduct = getOne(Product);
-export const updateProduct = updateOne(Product);
+
+export const updateProduct = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) return next(new AppError("Not finding product given id: ", 401));
+  if (product.seller !== req.user._id)
+    return next(new AppError("You are not created this product: ", 401));
+
+  const data = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    data: { data },
+  });
+});
