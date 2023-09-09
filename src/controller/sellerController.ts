14413c42757de18135f95389fc123b14a83cdf53
Hashib Raja <hashibraja23@gmail.com>
2023-09-09 17:53:26 +0530

    for getting seller with id checking if seller is exist or not

--- src/controller/sellerController.ts
+++ src/controller/sellerController.ts
@@ -4,15 +4,20 @@ import User from "../models/userModel";
 import { AppError } from "../utils/AppError";
 import { getAll } from "./handleFactory";
 
-export const getSeller = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+export const isSeller = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const { sellerId } = req.params;
   const seller = await User.findById(sellerId);
   if (!seller || !(seller.role === "seller"))
     return next(new AppError("Not find any seller with given Id", 404));
 
+  req.user = seller;
+  next();
+});
+
+export const getSeller = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
     status: "success",
-    data: { seller },
+    data: { seller: req.user },
   });
 });
 
--- src/routes/sellerRoutes.ts
+++ src/routes/sellerRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllSeller, getSeller, setSellerFilter } from "../controller/sellerController";
+import { getAllSeller, getSeller, isSeller, setSellerFilter } from "../controller/sellerController";
 import { protect, restrictTo } from "../controller/authController";
 
 const router = Router();
 
 // Accessible to All Users:
+router.use("/:sellerId", isSeller);
 router.route("/:sellerId").get(getSeller);
 
 // Admin Accessible:
