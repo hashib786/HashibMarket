0e9686fae0ac58b2d34a3a7cbe2ac19b64de5441
Hashib Raja <hashibraja23@gmail.com>
2023-09-11 17:28:06 +0530

    implemented update reveiw route

--- src/controller/reviewController.ts
+++ src/controller/reviewController.ts
@@ -1,4 +1,6 @@
 import Review from "../models/reviewModel";
-import { createOne } from "./handleFactory";
+import { checkingSameUser, createOne, updateOne } from "./handleFactory";
 
 export const createReview = createOne(Review);
+export const checkingSameReviewUser = checkingSameUser(Review);
+export const updateReview = updateOne(Review);
--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { protect, restrictTo } from "../controller/authController";
 import { setUserInBody } from "../controller/wishlistController";
-import { createReview } from "../controller/reviewController";
+import { checkingSameReviewUser, createReview, updateReview } from "../controller/reviewController";
 
 const router = Router();
 
 // Logged-In User Accessible:
 router.use("/", protect, restrictTo("user"));
 router.route("/").post(setUserInBody, createReview);
+
+router.use("/:id", checkingSameReviewUser);
+router.route("/:id").patch(updateReview);
 
 export default router;
